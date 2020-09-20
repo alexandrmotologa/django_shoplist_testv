41f6b544c90be3861fb179d6828b9d5eaa365483
alexandrmotologa <al.motologa@gmail.com>
2020-09-20 21:52:28 +0300

    .

--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from .models import Product
+from django.utils import timezone
 
 
 def home(request):
+    now = timezone.now().strftime("%Y-%m-%d %H:%M")
     data = {
         'product': Product.objects.all(),
-        'title': 'Home Page'
+        'title': 'Home Page',
+        'time': now
     }
     return render(request, 'shop/home.html', data)
 
 def order(request):
-    return render(request, 'shop/order.html')
+    now = timezone.now().strftime("%Y-%m-%d %H:%M")
+    data = {
+        'time': now
+    }
+    return render(request, 'shop/order.html', data)
